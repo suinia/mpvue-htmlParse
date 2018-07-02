140ce6cf3315affa5325c46db10b31ebb6927f03
suinia <suini_a@163.com>
2018-07-02 16:57:36 +0800

    feat: add html 2 json

--- htmlParse.vue
+++ htmlParse.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <block v-if="data && data.length" v-for="(item, index) in data" :key="index">
+    <block v-if="nodeList && nodeList.length" v-for="(item, index) in nodeList" :key="index">
       <htmlParse :item="item" />
     </block>
   </div>
@@ -8,13 +8,26 @@
 
 <script>
   import htmlParse from './components/htmlParse1';
-
+  import html2json from './lib/html2json/index';
   export default {
+    data() {
+      return {
+        nodeList: []
+      }
+    },
     props: {
-      data: null
+      data: null,
+      html: ''
     },
     components: {
       htmlParse
+    },
+    mounted() {
+      if (this.data) {
+        this.nodeList = this.data;
+      } else {
+        this.nodeList = html2json(this.html);
+      }
     }
   };
 </script>
